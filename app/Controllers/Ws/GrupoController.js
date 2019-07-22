fbed186dd178c9bcc728e4eabbc2fbf580bb4eb0
mavericc <escobarveric@gmail.com>
2019-07-22 00:09:31 -0500

    escribiendo

--- app/Controllers/Ws/GrupoController.js
+++ app/Controllers/Ws/GrupoController.js
@@ -1,11 +1,97 @@
 'use strict'
 
+const Grupo = use ('App/Models/Grupo');
+var grupos = [];
+
+
+
 class GrupoController {
+
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
 
-    console.log("HOLA ESTAMOS EN EL SOCKET PARGUELÓN");
+    // this.obtener();
+
+    console.log("------------------------");
+  }
+
+  async obtener() {
+    grupos = await Grupo.all();
+    console.log(grupos.toJSON());
+
+  }
+
+  onEscribiendo(data) {
+
+    if (grupos.length < 0) {
+      grupos.push({
+        grupo: data.grupo,
+        escriben: []
+      });
+
+      grupos.find((grupo) => {
+        if (grupo.grupo === data.grupo) {
+          grupo.escriben.push(data.usuario);
+        }
+      });
+    } else {
+      //variable con la cual checaremos si un grupo ya existe
+      var exists;
+      
+      grupos.find((grupo) => {  
+        exists = (grupo.grupo === data.grupo ? true : false);
+      });
+      
+      if (!exists) {
+        grupos.push({
+          grupo: data.grupo,
+          escriben: []
+        });
+      
+        grupos.find((grupo) => {
+          if (grupo.grupo === data.grupo) {
+            grupo.escriben.push(data.usuario);
+          }
+        });
+      } else {
+        grupos.find((grupo) => {
+          if (grupo.grupo === data.grupo) {
+            grupo.escriben.push(data.usuario);
+          }
+        });
+      }//fin if exists para revisar si hay un grupo que se busca
+    }
+
+    var unique = [];
+    grupos.find((grupo) => {
+      if (grupo.grupo === data.grupo) {
+        unique = grupo.escriben.filter(function(item, pos) {
+          return grupo.escriben.indexOf(item) == pos;
+        });
+      }
+    });
+    console.log('LOS QUE ESCRIBEN');
+    console.log(unique);
+    this.socket.broadcast('escribiendo', unique);
+  }
+
+  onNotescribiendo(data) {
+    var unique = [];
+
+    grupos.find((grupo) => {
+      if (grupo.grupo === data.grupo) {
+        unique = grupo.escriben.filter(function(item, pos) {
+          return item !== data.usuario;
+        });
+      };
+
+    });
+    this.socket.broadcastToAll('notescribiendo', unique);
+  }
+
+  onMessage(message) {
+    this.socket.broadcastToAll('message', message.usuario);
   }
 }
 
